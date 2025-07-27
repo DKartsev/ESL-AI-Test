b99fcae985e61059b397b6b1dd293c341bf0da7a
DKartsev <50330328+DKartsev@users.noreply.github.com>
2025-07-27 16:36:04 +0400

    � Удалены неиспользуемые вспомогательные файлы

--- filter_clean.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("translated_cefr_dataset_filtered.csv")
-df["cefr_level"] = df["cefr_level"].replace({"C1": "C", "C2": "C"})
-df_clean = df[df["is_suspicious"] == False].reset_index(drop=True)
-df_clean.to_csv("translated_cefr_dataset_clean_combined.csv", index=False)
-print("✅ Датасет сохранён.")
